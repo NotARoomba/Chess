bdf76b5682cace6df068c029c6ee39a077a45d16
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-07-29 18:57:18 -0500

    aja

---
+++ src/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+export default function Home() {
+
+  return (
+    <>
+    <NavBar/>
+    <div className='font-inter text-white text-center m-32 text-9xl'>
+      Chess
+    </div>
+    </>
+  )
+}
